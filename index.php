d66aeffc9d831c1e6b21c6164d3d07212d870c88
Matthijs Brouwer <matthijs@artandflywork.org>
2019-03-11 12:12:38 +0100

    cleanup

--- index.php
+++ index.php
@@ -6,12 +6,8 @@ $defaultModels = [];
 foreach ($blueprints as $bp) {
   $bp = str_replace('.yml', '', $bp);
   if (!file_exists(kirby()->roots()->models() . DS . $bp . '.php')) {
-    $defaultModels[$bp] = 'DefaultModel';
+    $defaultModels[$bp] = 'DefaultPage';
   }
-}
-
-class DefaultModel extends DefaultPage
-{
 }
 
 Kirby::plugin('reprovinci/defaultModels', [
